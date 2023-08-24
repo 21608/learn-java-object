a214b1ab8e301400a225fe3167a4e505f26a410d
21608 <oka00027@gmail.com>
2023-08-24 22:45:13 +0900

    fromメソッド追加

--- StaticFactory.java
+++ StaticFactory.java
@@ -3,11 +3,17 @@ public class StaticFactory {
         for(int x : Methods.of(1, 2, 3)) {
             System.out.println(x);
         }
+
+        Methods.from(new StaticFactory());
     }
 
     class Methods {
         public static int[] of(int ...params) { 
             return params; 
+        }
+
+        public static <T extends StaticFactory> T from(T param) {
+            return param;
         }
     }
 }
